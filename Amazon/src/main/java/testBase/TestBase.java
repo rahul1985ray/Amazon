64365638a0a0bc9b99e1da2b95da0eb4a2aa3682
Asit Ray <Asit Ray@dell>
2020-06-04 13:32:05 +0530

    test base updated

--- Amazon/src/main/java/testBase/TestBase.java
+++ Amazon/src/main/java/testBase/TestBase.java
@@ -2,6 +2,7 @@ package testBase;
 
 import java.io.FileInputStream;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -29,6 +30,10 @@ public class TestBase {
      if(browserName.equals("chrome")) {
     	 System.setProperty("webdriver.chrome.driver", "F:\\selenium\\chromedriver_win32");
     	 driver = new ChromeDriver();
+    	 driver.manage().window().maximize();
+    	 driver.manage().deleteAllCookies();
+    	 driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    	 driver.get(prop.getProperty("url"));
      }
      
 	}
